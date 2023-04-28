341921be35eb88b9c2e642534a4c284de089c040
Marcos Gabriel Lopes <marcos.lopes@cooperflora.com.br>
2023-04-28 17:53:33 -0300

    initial

---
+++ src/test/java/br/com/jurisconexao/hub/HubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jurisconexao.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
